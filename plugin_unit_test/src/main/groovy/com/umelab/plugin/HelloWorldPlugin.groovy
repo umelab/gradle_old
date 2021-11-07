d909bce0ffe2373d9de2714202282f1b5add59a3
KIP_UmedaEizo <eizo-umeda@kip.co.jp>
2021-11-07 09:31:24 +0900

    [add]simple unit test sample with a plugin

---
+++ plugin_unit_test/src/main/groovy/com/umelab/plugin/HelloWorldPlugin.groovy
@@ -0,0 +1,14 @@
+package com.umelab.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class HelloWorldPlugin implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('helloWorld') {
+            doLast {
+                println 'Hello world!'
+            }
+        }
+    }
+}
